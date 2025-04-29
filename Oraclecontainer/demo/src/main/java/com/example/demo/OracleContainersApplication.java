0c521cd4fe01026be73dabeb7700cbd9f66e79ee
Karen Corona Espinoza <110923327+KarenCorona@users.noreply.github.com>
2025-04-28 23:53:25 -0600

    Add files via upload

---
+++ Oraclecontainer/demo/src/main/java/com/example/demo/OracleContainersApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleContainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleContainersApplication.class, args);
+	}
+
+}
